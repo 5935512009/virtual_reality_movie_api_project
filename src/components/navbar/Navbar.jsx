19daf0e7d08d9cc65708c5f30f60dc61d85659a7
5935512009 <Pasut2541@hotmail.com>
2025-02-24 13:22:58 +0700

    updates:{import axios and get api }

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import './navbar.css'
 function Navbar() {
+
   return (
     <div className='nav'>
       <div className='nav-left'> 
--- src/pages/movie/Movies.jsx
+++ src/pages/movie/Movies.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import axios from 'axios';
 function Movies() {
-  return (
+const [items,setItems] =useState([]);    
+useEffect(()=>{
+    axios
+    .get('https://api.themoviedb.org/3/search/movie?api_key=de10c7a41b498f84b45c50b0ad40101d&query=a')
+    .then((res)=>{
+        setItems(res.data);
+        
+    })
+
+
+},[]);
+console.log(items);
+    return (
     <div>
       Movie
     </div>
