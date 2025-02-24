411559a4de04f4ee6b43d80375e8313f01f466ba
5935512009 <Pasut2541@hotmail.com>
2025-02-24 12:26:23 +0700

    updates:{basic page and react router dom}

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './navbar.css'
+function Navbar() {
+  return (
+    <div className='nav'>
+      <div className='nav-left'> 
+        Movie virtual reality
+        <ul>
+            <li>
+                <Link to="./">Home</Link>
+                <Link to="./movies">Movies</Link>
+            </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
